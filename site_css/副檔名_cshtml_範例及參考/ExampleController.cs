04fcfb5bac7e022fbd36d11d0945ee10a4c8f25f
Mogray65 <141423094+Mogray65@users.noreply.github.com>
2024-11-02 01:22:19 +0800

    style(project): add site.css & Demo, Font, Color, three pages in total

---
+++ site_css/副檔名_cshtml_範例及參考/ExampleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YUM_YUM_test.Controllers
+{
+	public class ExampleController : Controller
+	{
+		public IActionResult Demo()
+		{
+			return View();
+		}
+
+		public IActionResult Font()
+		{
+			return View();
+		}
+
+		public IActionResult Color()
+		{
+			return View();
+		}
+	}
+}
