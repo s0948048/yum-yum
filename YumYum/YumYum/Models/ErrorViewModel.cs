e6ae4995373607f16340f6e3a2472f2c6ef49c28
Chris Chou <tsunghaochou@gmail.com>
2024-11-01 10:14:50 +0800

    initialize: add initial NET Core MVC Template

---
+++ YumYum/YumYum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YumYum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
