6aaf9604e874b99cc9831fe31499c2aab9d2326e
FangTzu-Chiang <onion110324@gmail.com>
2024-11-06 17:23:59 +0800

    feat: add controller

---
+++ YumYum/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YumYum.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MyRecipeEdit()
+        {
+            return View();
+        }
+
+        public IActionResult MyRecipeCollect()
+        {
+            return View();
+        }
+    }
+}
