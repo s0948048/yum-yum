51e765bcbfc1aef1c8b8a1db61e23dd50f25e2bd
Hubery-1003 <k25298768@gmail.com>
2024-11-08 14:16:39 +0800

    feat:add User Views RegisterPage(cshtml,css,js)

---
+++ YumYum/Views/User/RegisterPage.cshtml.js
@@ -0,0 +1,16 @@
+﻿var Reg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})*$/;
+var Reg2=/^[\w]{6,}$/
+$('#register-submit').on("click", function () {
+    if (!Reg.test($('#register-email').val())) {
+        alert("信箱不符合格式，請重新輸入")
+    }
+    else if (!Reg2.test($('#register-pwd').val())) {
+        alert("密碼不符合格式，請重新輸入")
+    }
+    else if ($('#register-pwd').val() != $('#register-pwddb').val() ){
+        alert("確認密碼與註冊密碼不同，請重新輸入")
+    }
+    else {
+        window.location.href = 'User/LogInPage'
+    }
+})
