803bae19ab81ead2df74938df865eefcf0d5dad4
Hubery-1003 <k25298768@gmail.com>
2024-11-08 13:32:33 +0800

    refractor:User Controller.cs add action LogInPage and RegisterPage,import Database codes

--- YumYum/Controllers/UserController.cs
+++ YumYum/Controllers/UserController.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YumYum.Models;
 
 namespace YumYum.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        //引用資料庫
+        private readonly YumYumDbContext _context;
+
+        public UserController(YumYumDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
         {
             return View();
         }
+
+
+        //芳慈
         public IActionResult MyRecipeEdit()
         {
             return View();
@@ -17,6 +31,33 @@ namespace YumYum.Controllers
         {
             return View();
         }
-		
-	}
+
+
+        //毅祥
+        public async Task<IActionResult> LogInPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogInPage([FromBody] UserSecretInfo user)
+        {
+            UserSecretInfo users = await _context.UserSecretInfos.Where(p => p.Email == user.Email).FirstOrDefaultAsync();
+
+            if (users != null)
+            {
+                HttpContext.Session.SetInt32("userId", users.UserId);
+                return Json(new { redirectUrl = Url.Action("Index", "Home") });
+            }
+            else
+            {
+                return Json(new { errorMessage = "帳號未註冊" });
+            }
+        }
+
+        public async Task<IActionResult> RegisterPage()
+        {
+            return View();
+        }
+    }
 }
