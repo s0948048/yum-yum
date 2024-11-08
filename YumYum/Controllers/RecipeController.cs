a90c6b48bba76769fbc6d88251be7993a1cbf132
Max <99115855+maxzhugithuntest@users.noreply.github.com>
2024-11-08 14:25:27 +0800

    Add
    add RecipeController and Index(Home)

---
+++ YumYum/Controllers/RecipeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YumYum.Controllers
+{
+	public class RecipeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
