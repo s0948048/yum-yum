9f9c3e3bca858fb11fa7b790825deb1adeb9d163
Hubery-1003 <k25298768@gmail.com>
2024-11-09 18:54:09 +0800

    docs:Model Folder-add RecipeWatch.cs and IngerdientWatch.cs

---
+++ YumYum/Models/IngredientWatch.cs
@@ -0,0 +1,14 @@
+﻿namespace YumYum.Models
+{
+    public class IngredientWatch
+    {
+        //食材名稱
+        public string? ingredientName { get; set; }
+        //食材icon路徑
+        public string? ingredientImg { get; set; }
+        //食材數量
+        public string? ingredientCount { get; set; }
+        //食材單位名稱
+        public string? ingredientsUnit { get; set; }
+    }
+}
