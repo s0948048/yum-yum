0c82cf287faa84ca9ce6adc5699ceade6366a009
FangTzu-Chiang <onion110324@gmail.com>
2024-11-11 00:49:03 +0800

    feat:add breadcrumbitem class

---
+++ YumYum/Models/BreadcrumbItem.cs
@@ -0,0 +1,14 @@
+﻿namespace YumYum.Models
+{
+    public class BreadcrumbItem
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+
+        public BreadcrumbItem(string text, string url)
+        {
+            Text = text;
+            Url = url;
+        }
+    }
+}
