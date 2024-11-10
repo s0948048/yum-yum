c4dfb78de13f18fb0120fcdec45734f84116327f
Chris Chou <tsunghaochou@gmail.com>
2024-11-10 20:25:53 +0800

    feat: add FridgeController

---
+++ YumYum/Controllers/FridgeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YumYum.Controllers
+{
+    public class FridgeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
