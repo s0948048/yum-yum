a03589f52b310606f0fcb780183d42eb3fd138a9
Chris Chou <tsunghaochou@gmail.com>
2024-11-10 22:06:14 +0800

    feat: config to connect to DB

--- YumYum/Controllers/FridgeController.cs
+++ YumYum/Controllers/FridgeController.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using YumYum.Models;
 
 namespace YumYum.Controllers
 {
     public class FridgeController : Controller
     {
+        private readonly YumYumDbContext _context;
         public IActionResult Index()
         {
-            return View();
+            var viewModel = _context.RefrigeratorStores.ToList();
+            return View(viewModel);
         }
 
         public IActionResult Edit()
         {
             return View();
+        }
+        public FridgeController(YumYumDbContext context)
+        {
+            _context = context;
         }
     }
 }
