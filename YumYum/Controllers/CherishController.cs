48005c34771ae3e2eca93bbc74bfb55908aa5f2d
Mogray65 <141423094+Mogray65@users.noreply.github.com>
2024-11-10 19:33:34 +0800

    feat: add CherishController and /Views/Cherish/

---
+++ YumYum/Controllers/CherishController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YumYum.Controllers
+{
+	public class CherishController : Controller
+	{
+		public IActionResult Introduce()
+		{
+			return View();
+		}
+
+		public IActionResult Manage()
+		{
+			return View();
+		}
+
+		public IActionResult Match()
+		{
+			return View();
+		}
+
+		public IActionResult MatchHistory()
+		{
+			return View();
+		}
+
+		public IActionResult ContactInformation()
+		{
+			return View();
+		}
+	}
+}
