3db00f871d3f2f28c2b838df3f4d4c63e2b17892
Chris Chou <tsunghaochou@gmail.com>
2024-11-12 00:21:03 +0800

    feat: config viewModels, combine 2 sub-models

--- YumYum/Models/ViewModels/FridgeViewModel.cs
+++ YumYum/Models/ViewModels/FridgeViewModel.cs
@@ -2,5 +2,22 @@
 {
     public class FridgeViewModel
     {
+        public List<FridgeItemViewModel>? RefrigeratorData { get; set; }
+        public List<IngredientViewModel>? IngredientData { get; set; }
+    }
+
+    public class FridgeItemViewModel
+    {
+        public int UserID { get; set; }
+        public string? IngredientName { get; set; }
+        public string? Quantity { get; set; }
+        public string? UnitName { get; set; }
+        public DateOnly ValidDate { get; set; }
+    }
+
+    public class IngredientViewModel
+    {
+        public string? IngredientName { get; set; }
+        public string? IngredientIcon { get; set; }
     }
 }
