c25c3cc7219b1c95195b907d00dc28f71d4f30fd
Chris Chou <tsunghaochou@gmail.com>
2024-11-12 10:05:48 +0800

    feat: add corresponding IngredientIcon prop.

--- YumYum/Models/ViewModels/FridgeViewModel.cs
+++ YumYum/Models/ViewModels/FridgeViewModel.cs
@@ -10,6 +10,7 @@
     {
         public int UserID { get; set; }
         public string? IngredientName { get; set; }
+        public string? IngredientIcon { get; set; }
         public string? Quantity { get; set; }
         public string? UnitName { get; set; }
         public DateOnly ValidDate { get; set; }
