1c6e12d66fa189b130507eab63b962dbb899dfdd
Chris Chou <tsunghaochou@gmail.com>
2024-11-12 09:50:03 +0800

    fix: change fridge item sorting order

--- YumYum/Controllers/FridgeController.cs
+++ YumYum/Controllers/FridgeController.cs
@@ -13,7 +13,7 @@ namespace YumYum.Controllers
                              join igd in _context.Ingredients on fridge.IngredientId equals igd.IngredientId
                              join unit in _context.Units on fridge.UnitId equals unit.UnitId
                              where fridge.UserId == 3204 
-                             orderby fridge.Quantity
+                             orderby fridge.ValidDate
                              select new FridgeItemViewModel
                              {
                                  UserID = fridge.UserId,
