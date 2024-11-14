918679e283dc661560d612909dbe71e018b23015
Hubery-1003 <k25298768@gmail.com>
2024-11-14 15:40:49 +0800

    feat:User-RegisterPage(chtml,css,js),RegisterVerifyPage;Refractor:UserController,User-LoginPage(css)

--- YumYum/Views/User/RegisterPage.cshtml.js
+++ YumYum/Views/User/RegisterPage.cshtml.js
@@ -1,16 +1,54 @@
-﻿var Reg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})*$/;
-var Reg2=/^[\w]{6,}$/
+﻿var regEamil = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})*$/;
+var regPwd=/^(?=.*[a-zA-Z])(?=.*\d)[\w]{6,}$/
 $('#register-submit').on("click", function () {
-    if (!Reg.test($('#register-email').val())) {
+
+    if ($('#register-nickname').val() == '') {
+        alert("暱稱未填寫，請重新輸入")
+    }
+    else if (!regEamil.test($('#register-email').val())) {
         alert("信箱不符合格式，請重新輸入")
     }
-    else if (!Reg2.test($('#register-pwd').val())) {
+    else if (!regPwd.test($('#register-pwd').val())) {
         alert("密碼不符合格式，請重新輸入")
     }
     else if ($('#register-pwd').val() != $('#register-pwddb').val() ){
         alert("確認密碼與註冊密碼不同，請重新輸入")
     }
     else {
-        window.location.href = 'User/LogInPage'
+        //要傳遞的註冊資料
+        let data = {
+            UserNickname: $('#register-nickname').val(),
+            Email: $('#register-email').val(),
+            Password: $('#register-pwd').val(),
+        }
+        //傳遞資料給server->controller
+        $.ajax({
+            url: 'User/RegisterPage',
+            type: 'Post',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            success: function (result) {
+                console.log('成功')
+                if (result['registerdMessage'] != null) {
+                    alert(result['registerdMessage'])
+                }
+                else if (result['mailsuccess'] != null) {
+                    alert(result['mailsuccess'])
+                    window.location.href = result["action"];
+                }
+                else if (result['message'] != null) {
+                    alert(result['message'])
+                }
+                else {
+                    alert("未寄送成功信件")
+                }
+            },
+            error: function (xhr, status, error,) {
+                console.error('錯誤', error)
+            }
+        });
     }
+   
+    
+
 })
