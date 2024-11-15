a26fd743b1281abfa40a903ca11b3f030ad05597
Hubery-1003 <k25298768@gmail.com>
2024-11-15 12:53:01 +0800

    fix:Model-ingredientAttribute

--- YumYum/Models/IngredAttribute.cs
+++ YumYum/Models/IngredAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace YumYum.Models;
 
@@ -10,10 +11,10 @@ public partial class IngredAttribute
     public string IngredAttributeName { get; set; } = null!;
 
     public string IngredAttributePhoto { get; set; } = null!;
-
+    [JsonIgnore]
     public virtual ICollection<CherishOrder> CherishOrders { get; set; } = new List<CherishOrder>();
-
+    [JsonIgnore]
     public virtual ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
-
+    [JsonIgnore]
     public virtual ICollection<Unit> Units { get; set; } = new List<Unit>();
 }
