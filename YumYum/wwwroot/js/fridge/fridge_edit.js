b807e46288a2a7a093400aba89f211f7f687a2ce
Chris Chou <tsunghaochou@gmail.com>
2024-11-15 15:54:47 +0800

    feat: config offcanvas(Ifilter) toggle js

--- YumYum/wwwroot/js/fridge/fridge_edit.js
+++ YumYum/wwwroot/js/fridge/fridge_edit.js
@@ -1,3 +1,10 @@
 ﻿$("#btn-return").on("click", function () {
     window.location.href = "/fridge/index";
 });
+
+// Add Ajax HERE in this function!
+$('#Ifilter input[type="checkbox"]').on('change', function () {
+    console.log($(this).prop('checked'));
+    //console.log($('#Ifilter input[type="checkbox"]:checked'));
+    $(this).prev().toggleClass('selected');
+});
