735c2d91bc9251ba6efde2fe3b0c07438803fa62
Max <99115855+maxzhugithuntest@users.noreply.github.com>
2024-11-16 10:15:52 +0800

    Update user EditInfo page
    also update EditInfo.js

--- YumYum/wwwroot/js/user/EditInfo.js
+++ YumYum/wwwroot/js/user/EditInfo.js
@@ -1,33 +1,55 @@
 ﻿
-window.addEventListener('load', function () {
 
-	function updateCount() {
-		var cs = $(this).val().length;
-		if (this.id == "input-intro") {
-			$('#text-intro').text(`${cs}/200`);
+
+
+
+
+$(document).ready(
+	function () {
+		$("#btn-save").click(
+			function (event) {
+				var facebookUrl = $("#input-facebook-link").val();
+				var youtubeUrl = $("#input-youtube-link").val();
+				var facebookRegex = /^(http|https)\:\/\/(www\.)?facebook\.com\/.*$/;
+				var youtubeRegex = /^(http|https)\:\/\/(www\.)?youtube\.com\/.*$/;
+				var isValid = true;
+				if (facebookUrl !== "" && !facebookRegex.test(facebookUrl)) { alert("請輸入有效的Facebook網址"); isValid = false; }
+				if (youtubeUrl !== "" && !youtubeRegex.test(youtubeUrl)) { alert("請輸入有效的YouTube網址"); isValid = false; }
+				if (!isValid) { event.preventDefault(); }
+			});
+		function updateCount() {
+			var cs = $(this).val().length;
+			if (this.id == "input-intro") {
+				$('#text-intro').text(`${cs}/200`);
+			}
+			else if (this.id == "input-nickname") {
+				$('#text-nickname').text(`${cs}/16`);
+			}
 		}
-		else if (this.id == "input-nickname") {
-			$('#text-nickname').text(`${cs}/16`);
-		}
-	}
 
-	$('textarea').keyup(updateCount);
-	$('textarea').keydown(updateCount);
-	$('input').keyup(updateCount);
-	$('input').keydown(updateCount);
+		//函式綁定事件
+		$('textarea').keydown(updateCount);
+		$('textarea').keyup(updateCount);
+		$('textarea').on('paste', updateCount);
+		$('input').keydown(updateCount);
+		$('input').keyup(updateCount);
+		$('input').on('paste', updateCount);
 
-	$("#close-instagram").on('click', () => {
-		document.getElementById("instagram-edit").click();
-	})
-	$("#close-facebook").on('click', () => {
-		document.getElementById("facebook-edit").click();
-	})
-	$("#close-youtube").on('click', () => {
-		document.getElementById("youtube-edit").click();
-	})
-	$("#close-create").on('click', () => {
-		document.getElementById("create-edit").click();
-	})
+		$("#close-instagram").on('click', () => {
+			document.getElementById("instagram-edit").click();
+		})
+		$("#close-facebook").on('click', () => {
+			document.getElementById("facebook-edit").click();
+		})
+		$("#close-youtube").on('click', () => {
+			document.getElementById("youtube-edit").click();
+		})
+		$("#close-create").on('click', () => {
+			document.getElementById("create-edit").click();
+		})
+
+	});
 
 
-})
+
+
