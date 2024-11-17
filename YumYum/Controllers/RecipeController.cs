bdbe4ca898bacf6c9cd18054f49ae479b745c876
Hubery-1003 <k25298768@gmail.com>
2024-11-17 19:53:36 +0800

    feat:Recipe-RecipeCreate add function pushRecipe and RecipeDraft RecipreCreate(cshtml,css,js,RecipeController)

--- YumYum/Controllers/RecipeController.cs
+++ YumYum/Controllers/RecipeController.cs
@@ -11,10 +11,13 @@ namespace YumYum.Controllers
     {
         //導入資料庫內容
         private readonly YumYumDbContext _context;
+        //用來取得根目錄
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public RecipeController(YumYumDbContext context)
+        public RecipeController(YumYumDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         //Index
@@ -80,6 +83,8 @@ namespace YumYum.Controllers
             return View(AllList);
         }
 
+
+
         public async Task<IActionResult> CreateRecipe()
         {
             var ingredientList = await _context.Ingredients.ToListAsync();
@@ -92,12 +97,15 @@ namespace YumYum.Controllers
                                 IngredAttributeName = attri.IngredAttributeName,
                                 IngredAttributePhoto = attri.IngredAttributePhoto,
                             };
+            int? userId = HttpContext.Session.GetInt32("userId");
+
             var allList = new RecipeCreate()
             {
                 Ingredients = ingredientList,
                 className = classList,
                 units = unitList,
                 attributes = attribute.ToList(),
+                userId = userId,
             };
 
 
@@ -106,6 +114,165 @@ namespace YumYum.Controllers
 
         }
 
+
+        //接收創建食譜的資訊
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRecipe([FromBody] RecipeCreate_Save saveData)
+        {
+            if (saveData == null)
+            {
+                return Json(new { success = false, message = "我是null" });
+            }
+
+            saveData.recipeBrief.Creator = await _context.UserSecretInfos.FindAsync(saveData.recipeBrief.CreatorId);
+            saveData.recipeBrief.RecipeClass = await _context.RecipeClasses.FindAsync(saveData.recipeBrief.RecipeClassId);
+
+            if (saveData.recipeBrief.Creator == null || saveData.recipeBrief.RecipeClass == null)
+            {
+                return Json(new { success = false, message = "creator null" });
+            }
+
+            // 清理 ModelState
+
+            if (!TryValidateModel(saveData))
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors)
+                                               .Select(e => e.ErrorMessage)
+                                               .ToList();
+                return Json(new { success = false, message = errors });
+            }
+
+
+            _context.RecipeBriefs.Add(saveData.recipeBrief);
+            await _context.SaveChangesAsync();
+            //將食譜的id提出出來
+
+            var recipe = _context.RecipeBriefs.FirstOrDefault(p => p.RecipeName == saveData.recipeBrief.RecipeName && saveData.recipeBrief.CreateDate.Date == p.CreateDate.Date);
+            string recipeID = recipe!.RecipeId.ToString();
+            short recipeIDshort = recipe!.RecipeId;
+            //存取紀錄資料與食材資料
+            saveData.recipeIngredients.ForEach(p => p.RecipeId = recipeIDshort);
+            _context.RecipeIngredients.AddRange(saveData.recipeIngredients);
+            saveData.recipeRecord.RecipeId = recipeIDshort;
+            _context.RecipeRecords.Add(saveData.recipeRecord);
+            await _context.SaveChangesAsync();
+            //先將圖片存入img folder並將saveData的shot資料都改成相對路徑的格式
+            if (saveData.recipeRecordFields != null && saveData.recipeRecordFields.Count > 0)
+            {
+                //將圖片的資料取出來並去除前綴詞
+                //因為Icollection無法使用索引器方法，先將資料轉換成list
+                var RecipeRecordFieldsList = saveData.recipeRecordFields.ToList();
+                bool hasUnknownField = false;
+                for (int i = 0; i < RecipeRecordFieldsList.Count; i++)
+                {
+                    //設定前端未傳回的資料recipeid(後端食譜存進去才會有)
+                    RecipeRecordFieldsList[i].RecipeId = recipeIDshort;
+                    RecipeRecordFieldsList[i].RecipeFieldNavigation = await _context.RecipeFields.FindAsync(RecipeRecordFieldsList[i].RecipeField);
+                    string mimetype = string.Empty;
+                    //如果是預設圖片就不要存入資料夾
+                    if (RecipeRecordFieldsList[i].FieldShot == "/img/icon/AddPhoto.png")
+                    {
+                        RecipeRecordFieldsList[i].FieldShot = "/img/icon/AddPhoto.png";
+                    }
+                    else if (RecipeRecordFieldsList[i].FieldShot!.StartsWith("data:image/jpeg;base64,"))
+                    {
+                        if (i == 0)
+                        {
+                            mimetype = "image/jpeg";
+                            var fieldShot = RecipeRecordFieldsList[i].FieldShot!.Replace("data:image/jpeg;base64,", "");
+                            byte[] imageBytes = Convert.FromBase64String(fieldShot);
+                            //設定副檔名及檔名
+                            string folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "img/recipe/recipe_intro");
+                            string fileName = recipeID + '_' + i + ".jpg";
+                            string fullfilename = Path.Combine(folderPath, fileName);
+                            //將圖片存進資料夾
+                            System.IO.File.WriteAllBytes(fullfilename, imageBytes);
+                            //將saveData的資料存成相對路徑
+                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_step/{fileName}";
+                            //將步驟資料存進資料庫
+                            _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
+                        }
+                        else
+                        {
+                            mimetype = "image/jpeg";
+                            var fieldShot = RecipeRecordFieldsList[i].FieldShot!.Replace("data:image/jpeg;base64,", "");
+                            byte[] imageBytes = Convert.FromBase64String(fieldShot);
+                            //設定副檔名及檔名
+                            string folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "img/recipe/recipe_step");
+                            string fileName = recipeID + '_' + i + ".jpg";
+                            string fullfilename = Path.Combine(folderPath, fileName);
+                            //將圖片存進資料夾
+                            System.IO.File.WriteAllBytes(fullfilename, imageBytes);
+                            //將saveData的資料存成相對路徑
+                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_intro/{fileName}";
+                            //將步驟資料存進資料庫
+                            _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
+                        }
+                    }
+                    else if (RecipeRecordFieldsList[i].FieldShot!.StartsWith("data:image/png;base64,"))
+                    {
+                        if (i == 0)
+                        {
+                            mimetype = "image/png";
+                            var fieldShot = RecipeRecordFieldsList[i].FieldShot!.Replace("data:image/png;base64,", "");
+                            byte[] imageBytes = Convert.FromBase64String(fieldShot);
+                            //設定副檔名及檔名
+                            string folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "img/recipe/recipe_intro");
+                            string fileName = recipeID + '_' + i + ".png";
+                            string fullfilename = Path.Combine(folderPath, fileName);
+                            //將圖片存進資料夾
+                            System.IO.File.WriteAllBytes(fullfilename, imageBytes);
+                            //將saveData的資料存成相對路徑
+                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_intro/{fileName}";
+                            //將步驟資料存進資料庫
+                            _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
+                        }
+                        else
+                        {
+                            mimetype = "image/png";
+                            var fieldShot = RecipeRecordFieldsList[i].FieldShot!.Replace("data:image/png;base64,", "");
+                            byte[] imageBytes = Convert.FromBase64String(fieldShot);
+                            //設定副檔名及檔名
+                            string folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "img/recipe/recipe_step");
+                            string fileName = recipeID + '_' + i + ".png";
+                            string fullfilename = Path.Combine(folderPath, fileName);
+                            //將圖片存進資料夾
+                            System.IO.File.WriteAllBytes(fullfilename, imageBytes);
+                            //將saveData的資料存成相對路徑
+                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_step/{fileName}";
+                            //將步驟資料存進資料庫
+                            _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
+                        }
+
+                    }
+                    else
+                    {
+                        hasUnknownField = true;
+                    }
+                    if (hasUnknownField)
+                    {
+                        return Json(new { success = "出現未知錯誤", fieldShot = RecipeRecordFieldsList[i].FieldShot });
+                    }
+                }
+            }
+            //存取紀錄、步驟、食材資料
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                return Json(new { success = false, message = "資料儲存失敗", error = ex.InnerException?.Message ?? ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "系統錯誤", error = ex.Message });
+            }
+
+            //回傳新建食譜成功
+            return Json(new { success = "新建食譜成功" });
+        }
 
 
         //編輯食譜 js css還暫套跟createRecipe一樣的之後差別是還要導入查看食譜的資料
@@ -134,6 +301,7 @@ namespace YumYum.Controllers
             return View(allList);
 
         }
+
 
 
         //健誠
