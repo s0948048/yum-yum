79f679dfec7e0d106f8a884fbd4c2073c1fb609d
Chris Chou <tsunghaochou@gmail.com>
2024-11-17 17:48:50 +0800

    feat: add AJAX for updating ingred tag

--- YumYum/wwwroot/js/fridge/fridge_edit.js
+++ YumYum/wwwroot/js/fridge/fridge_edit.js
@@ -1,11 +1,49 @@
-﻿$("#btn-return").on("click", function () {
+﻿const { type, error } = require("jquery");
+
+$("#btn-return").on("click", function () {
     window.location.href = "/fridge/index";
 });
 
-// Add Ajax HERE in this function!
+// Toggle selected class effect
 $('#Ifilter input[type="checkbox"]').on('change', function () {
-    //console.log($(this).prop('checked'));
-    //console.log($('#Ifilter input[type="checkbox"]:checked'));
-    //console.log($(this).prev());
     $(this).prev().toggleClass('selected');
+});
+
+$('.ingredient-attribute-checkbox').on('change', function () {
+    // Get all selected ID
+    let selectedId = [];
+    $('.ingredient-attribute-checkbox:checked').each(function () {
+        selectedId.push($(this).val());
+    });
+
+    // Ajax request to get filtered tags
+    $.ajax(
+        {
+            url: '@Url.Action("FilterIngredients", "Fridge")',
+            type: 'POST',
+            data: JSON.stringify(selectedId),
+            contentType: 'application/json',
+            success: function (response) {
+                // Update tag section dynamically
+
+                // Clear section
+                $('#ingredient-list').empty();
+
+                // Update
+                $.each(response, function (index, item) {
+                    $('#ingredient-list').append(
+                        `
+                        <button class="tag-button d-inline-flex align-items-center btn btn-tags">
+                                <img src="${item.IngredientIcon}" alt="" />
+                                <span>${item.IngredientName}</span>
+                        </button>
+                        `
+                    );
+                });
+            },
+            error: function (xhr, status, error) {
+                console.error('Error: ' + error);
+            }
+        }
+    );
 });
