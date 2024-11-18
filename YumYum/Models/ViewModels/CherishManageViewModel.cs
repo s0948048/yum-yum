2a839bcb6e7e523c695230225f765edcb602f112
Mogray65 <141423094+Mogray65@users.noreply.github.com>
2024-11-19 05:05:07 +0800

    feat: add CherishManageViewModel.cs

---
+++ YumYum/Models/ViewModels/CherishManageViewModel.cs
@@ -0,0 +1,49 @@
+﻿namespace YumYum.Models.ViewModels
+{
+	public class CherishManageViewModel
+	{
+		// [訂單類]
+		// 使用者編號(食材提供者)
+		public int GiverUserId { get; set; }
+
+		// 數量
+		public short Quantity { get; set; }
+
+		// 截止日期
+		public DateOnly EndDate { get; set; }
+
+		// 食材來源
+		public string? ObtainSource { get; set; }
+
+		// 購買日期
+		public DateOnly ObtainDate { get; set; }
+
+		// 有效期限
+		public DateOnly? CherishValidDate { get; set; }
+
+		// 圖片路徑
+		public string? CherishPhoto { get; set; }
+
+		// [食材類]
+		// 食材名稱
+		public string? IngredientName { get; set; }
+		// 食材分類
+		public string? IngredAttributeName { get; set; }
+
+		// [地區類]
+		// 縣市
+		public string? CityName { get; set; }
+		// 鄉鎮市區
+		public string? RegionName { get; set; }
+
+		// [聯絡類]
+		// 暱稱
+		public string? UserNickname { get; set; }
+		// 聯絡方式
+		public string? ContactLine { get; set; }
+		public string? ContactPhone { get; set; }
+		public string? ContactOther { get; set; }
+		// 面交時段
+		//public string? TradeTimeCode { get; set; }
+	}
+}
