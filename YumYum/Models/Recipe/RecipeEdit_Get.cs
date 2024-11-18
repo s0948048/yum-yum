526ee02423c01a9b8f21cc6b21670c80d09a774a
Hubery-1003 <k25298768@gmail.com>
2024-11-19 00:09:18 +0800

    feat:add Model EditRecipe_Get EditRecipe_Ingredient

---
+++ YumYum/Models/Recipe/RecipeEdit_Get.cs
@@ -0,0 +1,20 @@
+﻿namespace YumYum.Models.Recipe
+{
+    public class RecipeEdit_Get
+    {
+        //拿到食譜資料
+        public List<RecipeWatch_Brief>? recipeWatchBrief { get; set; }
+        public List<RecipeRecordField>? recipeRecordFields { get; set; }
+        public List<RecipeEdit_Ingredient>? recipeIngredient { get; set; }
+        //拿到全部的資歷庫食材、單位、食譜類別、食材類別資料
+        public List<Ingredient>? Ingredients { get; set; }
+        public List<RecipeClass>? className { get; set; }
+        public List<Unit>? units { get; set; }
+        public List<RecipeCreate_attribute>? attributes { get; set; }
+
+        public int? userId { get; set; }
+        //版本號及食譜狀態
+        public byte ? reipeVersion { get; set; }
+        public byte? recipeStatusCode { get; set; }
+    }
+}
---
+++ YumYum/Models/Recipe/RecipeEdit_Ingredient.cs
@@ -0,0 +1,22 @@
+﻿namespace YumYum.Models.Recipe
+{
+    public class RecipeEdit_Ingredient
+    {
+        //食材id
+        public int? ingredientId { get; set; }
+        //食材類型id
+        public int? ingredientAttribute { get; set; }
+
+        //食材名稱
+        public string? ingredientName { get; set; }
+        //食材icon
+        public string? ingredientImg { get; set; }
+        //食材數量
+        public string? ingredientCount { get; set; }
+        //食材單位ID
+        public int? unitId { get; set; }
+
+        //食材單位
+        public string? ingredientunit { get; set; }
+    }
+}
