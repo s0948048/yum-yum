cfab1e14688f7bf063c04becd120067233679cfa
Hubery-1003 <k25298768@gmail.com>
2024-11-19 00:20:44 +0800

    feat:EditRecipe ALL Function(cshtml,css,js,Controller-Recipe)

--- YumYum/Controllers/RecipeController.cs
+++ YumYum/Controllers/RecipeController.cs
@@ -34,7 +34,7 @@ namespace YumYum.Controllers
             //接收食譜的id
             int? recipeId = HttpContext.Session.GetInt32("recipeId");
             //測試(之後會刪掉)->
-            int recipeIdTest = 1402;
+            int recipeIdTest = 1488;
             //得到資料
             //取得食譜內容
             var recipeBrief = from recipe in await _context.RecipeBriefs.Where(p => p.RecipeId == recipeIdTest).ToListAsync()
@@ -54,7 +54,7 @@ namespace YumYum.Controllers
             //取得最新版本號
             var recipeStepAndIntroVersion = await _context.RecipeRecordFields.Where(p => p.RecipeId == recipeIdTest).MaxAsync(x => x.RecipeRecVersion);
             //取得最新版本的內容(簡介、食譜圖片、步驟圖片內容)
-            var recipeStepAndIntro = await _context.RecipeRecordFields.Where(p => p.RecipeId == recipeIdTest && p.RecipeRecVersion == recipeStepAndIntroVersion).ToListAsync();
+            var recipeStepAndIntro = await _context.RecipeRecordFields.Where(p => p.RecipeId == recipeIdTest && p.RecipeRecVersion == recipeStepAndIntroVersion).OrderBy(p => p.RecipeField).ToListAsync();
             //取得食譜食材
             var recipeIngredient = from data in await _context.RecipeIngredients.ToListAsync()
                                    join ingre in await _context.Ingredients.ToListAsync()
@@ -206,7 +206,7 @@ namespace YumYum.Controllers
                             //將圖片存進資料夾
                             System.IO.File.WriteAllBytes(fullfilename, imageBytes);
                             //將saveData的資料存成相對路徑
-                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_step/{fileName}";
+                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_intro/{fileName}";
                             //將步驟資料存進資料庫
                             _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
                         }
@@ -222,7 +222,7 @@ namespace YumYum.Controllers
                             //將圖片存進資料夾
                             System.IO.File.WriteAllBytes(fullfilename, imageBytes);
                             //將saveData的資料存成相對路徑
-                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_intro/{fileName}";
+                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_step/{fileName}";
                             //將步驟資料存進資料庫
                             _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
                         }
@@ -292,9 +292,60 @@ namespace YumYum.Controllers
         }
 
 
-        //編輯食譜 js css還暫套跟createRecipe一樣的之後差別是還要導入查看食譜的資料
+        //編輯食譜
         public async Task<IActionResult> EditRecipe()
         {
+            //以下是拿食譜資訊
+            //此區與此區的view更動中
+            //接收食譜的id
+            int? recipeId = HttpContext.Session.GetInt32("recipeId");
+            //測試(之後會刪掉)->
+            int recipeIdTest = 1565;
+            //得到資料
+            //取得食譜內容
+            var recipeBrief = from recipe in await _context.RecipeBriefs.Where(p => p.RecipeId == recipeIdTest).ToListAsync()
+                              join className in await _context.RecipeClasses.ToListAsync()
+                              on recipe.RecipeClassId equals className.RecipeClassId
+                              select new RecipeWatch_Brief
+                              {
+                                  //食譜名稱
+                                  recipeName = recipe.RecipeName,
+                                  //完成時間
+                                  recipeFinishTime = recipe.FinishMinute,
+                                  //食用人數
+                                  recipePeople = recipe.PersonQuantity,
+                                  //食譜類別
+                                  className = className.RecipeClassName,
+                              };
+            //取得最新版本號
+            var recipeStepAndIntroVersion = await _context.RecipeRecordFields.Where(p => p.RecipeId == recipeIdTest).MaxAsync(x => x.RecipeRecVersion);
+            //取得最新版本號之食譜狀態碼
+            var recipeNewestRecord = await _context.RecipeRecords.FirstOrDefaultAsync(p => p.RecipeId == recipeIdTest && p.RecipeRecVersion == recipeStepAndIntroVersion);
+            var recipeNewestRecordStatus = recipeNewestRecord.RecipeStatusCode;
+            //取得最新版本的內容(簡介、食譜圖片、步驟圖片內容)
+            var recipeStepAndIntro = await _context.RecipeRecordFields.Where(p => p.RecipeId == recipeIdTest && p.RecipeRecVersion == recipeStepAndIntroVersion).OrderBy(p => p.RecipeField).ToListAsync();
+            //取得食譜食材
+            var recipeIngredient = from data in await _context.RecipeIngredients.ToListAsync()
+                                   where data.RecipeId == recipeIdTest
+                                   select new RecipeEdit_Ingredient
+                                   {
+                                       //食材id
+                                       ingredientId = data.Ingredient.IngredientId,
+                                       //食材類型
+                                       ingredientAttribute = data.Ingredient.AttributionId,
+                                       //食材名稱
+                                       ingredientName = data.Ingredient.IngredientName,
+                                       //食材icon
+                                       ingredientImg = data.Ingredient.IngredientIcon,
+                                       //食材數量
+                                       ingredientCount = data.Quantity,
+                                       //食材單位Id
+                                       unitId = data.Unit.UnitId,
+                                       //食材單位
+                                       ingredientunit = data.Unit.UnitName,
+                                   };
+
+            //以下是拿食材、類別、單位資訊
             var ingredientList = await _context.Ingredients.ToListAsync();
             var classList = await _context.RecipeClasses.ToListAsync();
             var unitList = await _context.Units.ToListAsync();
@@ -305,12 +356,24 @@ namespace YumYum.Controllers
                                 IngredAttributeName = attri.IngredAttributeName,
                                 IngredAttributePhoto = attri.IngredAttributePhoto,
                             };
-            var allList = new RecipeCreate()
+            int? userId = HttpContext.Session.GetInt32("userId");
+
+            var allList = new RecipeEdit_Get()
             {
+                //資料庫所有關於食材、食譜類型、單位、食材屬性的資料
                 Ingredients = ingredientList,
                 className = classList,
                 units = unitList,
                 attributes = attribute.ToList(),
+                //要修改的食譜資料
+                userId = userId,
+                recipeWatchBrief = recipeBrief.ToList(),
+                recipeRecordFields = recipeStepAndIntro.ToList(),
+                recipeIngredient = recipeIngredient.ToList(),
+                //版本號及狀態碼
+                reipeVersion = recipeStepAndIntroVersion,
+                recipeStatusCode = recipeNewestRecordStatus,
+
             };
 
 
@@ -319,6 +382,203 @@ namespace YumYum.Controllers
 
         }
 
+
+        //接收創建食譜的資訊
+
+        [HttpPost]
+        public async Task<IActionResult> EditRecipe([FromBody] RecipeCreate_Save saveData)
+        {
+            if (saveData == null)
+            {
+                return Json(new { success = false, message = "我是null" });
+            }
+
+            saveData.recipeBrief.Creator = await _context.UserSecretInfos.FindAsync(saveData.recipeBrief.CreatorId);
+            saveData.recipeBrief.RecipeClass = await _context.RecipeClasses.FindAsync(saveData.recipeBrief.RecipeClassId);
+
+            if (saveData.recipeBrief.Creator == null || saveData.recipeBrief.RecipeClass == null)
+            {
+                return Json(new { success = false, message = "creator null" });
+            }
+
+            // 清理 ModelState
+
+            if (!TryValidateModel(saveData))
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors)
+                                               .Select(e => e.ErrorMessage)
+                                               .ToList();
+                return Json(new { success = false, message = errors });
+            }
+
+
+            _context.RecipeBriefs.Update(saveData.recipeBrief);
+            await _context.SaveChangesAsync();
+            //將食譜的id提出出來
+            var recipe = _context.RecipeBriefs.FirstOrDefault(p => p.RecipeName == saveData.recipeBrief.RecipeName && saveData.recipeBrief.CreateDate.Date == p.CreateDate.Date);
+            string recipeID = recipe!.RecipeId.ToString();
+            short recipeIDshort = recipe!.RecipeId;
+            //如果是自建食材，就新增食材進入資料庫
+            for (int i = 0; i < saveData.recipeIngredients.Count; i++)
+            {
+                saveData.recipeIngredients[i].RecipeId = recipeIDshort;
+                if (saveData.recipeIngredients[i].IngredientId == 0)
+                {
+                    Ingredient ingredient = new Ingredient()
+                    {
+                        IngredientName = saveData.ingredientNames[i],
+                        AttributionId = 9,
+                        IngredientIcon = "/img/icon/EmptyTag.svg"
+                    };
+                    _context.Ingredients.Add(ingredient);
+                    await _context.SaveChangesAsync();
+                    Ingredient data = await _context.Ingredients.FirstOrDefaultAsync(p => p.IngredientName == saveData.ingredientNames[i] && p.AttributionId == 9);
+                    saveData.recipeIngredients[i].IngredientId = data.IngredientId;
+                }
+            }
+            //存取紀錄資料與食材資料
+
+            var myIngredientDatas = await _context.RecipeIngredients.Where(p => p.RecipeId == recipeIDshort).ToDictionaryAsync(p => p.IngredientId);
+            foreach (var data in saveData.recipeIngredients)
+            {
+
+                if (myIngredientDatas.TryGetValue(data.IngredientId, out var myIngredientData))
+                {
+                    myIngredientData.Quantity = data.Quantity;
+                    myIngredientData.UnitId = data.UnitId;
+
+                }
+                else
+                {
+                    _context.RecipeIngredients.Add(data);
+                }
+            }
+            saveData.recipeRecord!.RecipeId = recipeIDshort;
+            _context.RecipeRecords.Add(saveData.recipeRecord);
+            await _context.SaveChangesAsync();
+
+            if (saveData.recipeRecordFields != null && saveData.recipeRecordFields.Count > 0)
+            {
+                //將圖片的資料取出來並去除前綴詞
+                //因為Icollection無法使用索引器方法，先將資料轉換成list
+                var RecipeRecordFieldsList = saveData.recipeRecordFields.OrderBy(p => p.RecipeField).ToList();
+                bool hasUnknownField = false;
+                for (int i = 0; i < RecipeRecordFieldsList.Count; i++)
+                {
+                    //設定前端未傳回的資料recipeid(後端食譜存進去才會有)
+                    RecipeRecordFieldsList[i].RecipeId = recipeIDshort;
+                    RecipeRecordFieldsList[i].RecipeFieldNavigation = await _context.RecipeFields.FindAsync(RecipeRecordFieldsList[i].RecipeField);
+                    string mimetype = string.Empty;
+                    //如果是預設圖片就不要存入資料夾
+                    if (RecipeRecordFieldsList[i].FieldShot == "/img/icon/AddPhoto.png")
+                    {
+                        RecipeRecordFieldsList[i].FieldShot = "/img/icon/AddPhoto.png";
+                        _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
+                    }
+                    else if (RecipeRecordFieldsList[i].FieldShot.StartsWith("/img"))
+                    {
+                        RecipeRecordFieldsList[i].FieldShot = RecipeRecordFieldsList[i].FieldShot;
+                        _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
+                    }
+                    else if (RecipeRecordFieldsList[i].FieldShot!.StartsWith("data:image/jpeg;base64,"))
+                    {
+                        if (i == 0)
+                        {
+                            mimetype = "image/jpeg";
+                            var fieldShot = RecipeRecordFieldsList[i].FieldShot!.Replace("data:image/jpeg;base64,", "");
+                            byte[] imageBytes = Convert.FromBase64String(fieldShot);
+                            //設定副檔名及檔名
+                            string folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "img/recipe/recipe_intro");
+                            string fileName = recipeID + '_' + i + ".jpg";
+                            string fullfilename = Path.Combine(folderPath, fileName);
+                            //將圖片存進資料夾
+                            System.IO.File.WriteAllBytes(fullfilename, imageBytes);
+                            //將saveData的資料存成相對路徑
+                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_intro/{fileName}";
+                            //將步驟資料存進資料庫
+                            _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
+                        }
+                        else
+                        {
+                            mimetype = "image/jpeg";
+                            var fieldShot = RecipeRecordFieldsList[i].FieldShot!.Replace("data:image/jpeg;base64,", "");
+                            byte[] imageBytes = Convert.FromBase64String(fieldShot);
+                            //設定副檔名及檔名
+                            string folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "img/recipe/recipe_step");
+                            string fileName = recipeID + '_' + i + ".jpg";
+                            string fullfilename = Path.Combine(folderPath, fileName);
+                            //將圖片存進資料夾
+                            System.IO.File.WriteAllBytes(fullfilename, imageBytes);
+                            //將saveData的資料存成相對路徑
+                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_step/{fileName}";
+                            //將步驟資料存進資料庫
+                            _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
+                        }
+                    }
+                    else if (RecipeRecordFieldsList[i].FieldShot!.StartsWith("data:image/png;base64,"))
+                    {
+                        if (i == 0)
+                        {
+                            mimetype = "image/png";
+                            var fieldShot = RecipeRecordFieldsList[i].FieldShot!.Replace("data:image/png;base64,", "");
+                            byte[] imageBytes = Convert.FromBase64String(fieldShot);
+                            //設定副檔名及檔名
+                            string folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "img/recipe/recipe_intro");
+                            string fileName = recipeID + '_' + i + ".png";
+                            string fullfilename = Path.Combine(folderPath, fileName);
+                            //將圖片存進資料夾
+                            System.IO.File.WriteAllBytes(fullfilename, imageBytes);
+                            //將saveData的資料存成相對路徑
+                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_intro/{fileName}";
+                            //將步驟資料存進資料庫
+                            _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
+                        }
+                        else
+                        {
+                            mimetype = "image/png";
+                            var fieldShot = RecipeRecordFieldsList[i].FieldShot!.Replace("data:image/png;base64,", "");
+                            byte[] imageBytes = Convert.FromBase64String(fieldShot);
+                            //設定副檔名及檔名
+                            string folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "img/recipe/recipe_step");
+                            string fileName = recipeID + '_' + i + ".png";
+                            string fullfilename = Path.Combine(folderPath, fileName);
+                            //將圖片存進資料夾
+                            System.IO.File.WriteAllBytes(fullfilename, imageBytes);
+                            //將saveData的資料存成相對路徑
+                            RecipeRecordFieldsList[i].FieldShot = $"/img/recipe/recipe_step/{fileName}";
+                            //將步驟資料存進資料庫
+                            _context.RecipeRecordFields.Add(RecipeRecordFieldsList[i]);
+                        }
+
+                    }
+                    else
+                    {
+                        hasUnknownField = true;
+                    }
+                    if (hasUnknownField)
+                    {
+                        return Json(new { success = "出現未知錯誤", fieldShot = RecipeRecordFieldsList[i].FieldShot });
+                    }
+                }
+            }
+            //存取紀錄、步驟、食材資料
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex)
+            {
+                return Json(new { success = false, message = "資料儲存失敗", error = ex.InnerException?.Message ?? ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = "系統錯誤", error = ex.Message });
+            }
+
+
+            //回傳新建食譜成功
+            return Json(new { success = "新建食譜成功" });
+        }
 
 
         //健誠
---
+++ YumYum/Views/Recipe/EditRecipe.cshtml.js
@@ -0,0 +1,141 @@
+﻿//字數計數
+$('#step-text01').on('input', function () {
+    $('#step-count01').text(`${$('#step-text01').val().length}/100`);
+})
+
+$('#step-text02').on('input', function () {
+    $('#step-count02').text(`${$('#step-text02').val().length}/100`);
+})
+
+$('#recipe-introduction').on('input', function () {
+    $('#recipe-introcount').text(`${$('#recipe-introduction').val().length}/100`);
+})
+
+$('#recipe-name').on('input', function () {
+    $('#recipe-namecount').text(`${$('#recipe-name').val().length}/15`);
+})
+
+
+
+//接收步驟的變數
+var steps = $('#all-steps').find('.card').length;
+
+
+$('#step-plus').on('click', function () {
+    steps += 1;
+    //如果step是偶數的話就在right-steps下append新的步驟框
+    if (steps == 1) {
+        var stepLeft = $('<div>', {
+            id: `step0${steps}`,
+            class: 'card mt-5 step',    // 設定 class 屬性
+            style: 'width: 18rem; margin-left: 7rem',
+            html: `<img b-wbveynjuki  src="/img/icon/AddPhoto.png" class="card-img-top img-load" alt="哈哈" style="height: 13rem;"><div b-wbveynjuki class= "text-center m-0 step-bk"><p b-wbveynjuki class="m-0 mt-1" style="font-size: 1.5rem;">STEP 0${steps}</p></div ><div b-wbveynjuki class="card-body" style="width:286px;height:152px"><textarea b-wbveynjuki id="step-text0${steps}" class="card-text" style="width:100%;height:100%;border-color:transparent" placeholder="點我輸入內容"></textarea></div>`
+        }).attr('b-wbveynjuki', '');
+        //把前一個步驟的mb-200屬性移除
+        $(`#step-countdiv0${steps - 1}`).removeClass('mb-200')
+        //中間連線
+        $('#left-steps').append('<div b-wbveynjuki style="position: absolute;margin-top: 15.2rem;margin-left: 24.7rem;color:var(--yum-secondary-4)"> ─────────────<img b-b-wbveynjuki class="remove-steps" style="width:50px;height:50px" src="/img/icon/close.svg"/></div>')
+        //步驟本人
+        $('#left-steps').append(stepLeft);
+        //計數
+        $('#left-steps').append(`<div id="step-countdiv0${steps}" class="d-flex mb-200" style="margin-right: 260px"><label id ="step-count0${steps}" class="d-flex ms-auto">0/100</label></div>`);
+        //新增計數邏輯
+        $('#left-steps').append(`<script>$('#step-text0${steps}').on('input', function () {$('#step-count0${steps}').text(\`\${$('#step-text0${steps}').val().length}/100\`);}) </script>`)
+    }
+    else if (steps % 2 == 0 && steps != 0) {
+
+        var stepRight = $('<div>', {
+            id: `step0${steps}`,
+            class: 'card step-right step',    // 設定 class 屬性
+            html: `<img b-wbveynjuki src="/img/icon/AddPhoto.png" class="card-img-top img-load" alt="哈哈" style="height: 13rem;"><div b-wbveynjuki class= "text-center m-0 step-bk"> <p b-wbveynjuki class="m-0 mt-1" style="font-size: 1.5rem;">STEP 0${steps}</p></div ><div b-wbveynjuki class="card-body" style="width:286px;height:152px"><textarea b-wbveynjuki id=step-text0${steps} class="card-text" style="width:100%;height:100%;border-color:transparent" placeholder="點我輸入內容"></textarea></div>`
+        }).attr('b-wbveynjuki', '');
+        //把前一個步驟的mb-200屬性移除
+        $(`#step-countdiv0${steps - 1}`).removeClass('mb-200')
+        //中間連線
+        $('#right-steps').append('<div b-wbveynjuki style="position: absolute;margin-top: 50.2rem;margin-left:-26px;color:var(--yum-secondary-4)"><img b-wbveynjuki class="remove-steps" style="width:50px;height:50px" src="/img/icon/close.svg" />─────────────</div>')
+        //步驟本人
+        $('#right-steps').append(stepRight);
+        //計數
+        $('#right-steps').append(`<div b-wbveynjuki class="d-flex mb-200" id="step-countdiv0${steps}" style="margin-right:146px"><label b-wbveynjuki id ="step-count0${steps}" class= "d-flex ms-auto">0/100</label></div>`);
+        //新增計數邏輯
+        $('#right-steps').append(`<script>$('#step-text0${steps}').on('input', function () {$('#step-count0${steps}').text(\`\${$('#step-text0${steps}').val().length}/100\`);}) </script>`)
+        //如果step是奇數的話就在left-steps下append新的步驟框
+    } else {
+        var stepLeft = $('<div>', {
+            id: `step0${steps}`,
+            class: 'card step-left step',    // 設定 class 屬性
+            html: `<img b-wbveynjuki  src="/img/icon/AddPhoto.png" class="card-img-top img-load" alt="哈哈" style="height: 13rem;"><div b-wbveynjuki class= "text-center m-0 step-bk"><p b-wbveynjuki class="m-0 mt-1" style="font-size: 1.5rem;">STEP 0${steps}</p></div ><div b-wbveynjuki class="card-body" style="width:286px;height:152px"><textarea b-wbveynjuki id="step-text0${steps}" class="card-text" style="width:100%;height:100%;border-color:transparent" placeholder="點我輸入內容"></textarea></div>`
+        }).attr('b-wbveynjuki', '');
+        //把前一個步驟的mb-200屬性移除
+        $(`#step-countdiv0${steps - 1}`).removeClass('mb-200')
+        //中間連線
+        $('#left-steps').append('<div b-wbveynjuki style="position: absolute;margin-top: 50.2rem;margin-left: 24.7rem;color:var(--yum-secondary-4)"> ─────────────<img b-mc3u0u1te1 class="remove-steps" style="width:50px;height:50px" src="/img/icon/close.svg"/></div>')
+        //步驟本人
+        $('#left-steps').append(stepLeft);
+        //計數
+        $('#left-steps').append(`<div id="step-countdiv0${steps}" class="d-flex mb-200" style="margin-right: 260px"><label id ="step-count0${steps}" class="d-flex ms-auto">0/100</label></div>`);
+        //新增計數邏輯
+        $('#left-steps').append(`<script>$('#step-text0${steps}').on('input', function () {$('#step-count0${steps}').text(\`\${$('#step-text0${steps}').val().length}/100\`);}) </script>`)
+    }
+
+
+})
+
+//刪除步驟邏輯
+$(document).on('click', '.remove-steps', function () {
+    steps -= 1;
+    console.log(steps)
+    //取得現在刪除的步驟幾
+    var step = $(this).parent().next().attr('id').slice(-1)
+    let stepInt = parseInt(step)
+    //取得總步驟數也是最後一步的數字
+    var stepCount = $('#all-steps').find('.step').length;
+    let stepCountInt = parseInt(stepCount)
+    //要執行迴圈的次數
+    let forTimes = stepCountInt - stepInt
+    //for迴圈
+    for (let i = 0; i < forTimes; i++) {
+        //將現在所在的元素找出來->將下一個步驟的內容替換現在的步驟
+        //取得下一個步驟的內容
+        console.log("我我我")
+        var nextStepText = $(`#step0${stepInt + i + 1}`).find('textarea').val();
+        //將現在步驟內容換成下一個步驟的內容
+        var thisStepText = $(`#step0${stepInt + i}`).find('textarea').val(nextStepText);
+        //------------------------------------------------------------------------------
+        //取得下一個步驟的照片內容
+        var nextStepImg = $(`#step0${stepInt + i + 1}`).find('img').attr('src');
+        //步驟照片
+        var thisStepImg = $(`#step0${stepInt + i}`).find('img').attr('src', nextStepImg);
+    }
+    //最後一個步驟刪除
+    //把他的上一個div刪除也就是中間連線的部分
+    $(`#step0${stepCount}`).prev().remove()
+    //把他的下一個div刪除也就是計數
+    $(`#step0${stepCount}`).next().remove()
+    //把自己刪除
+    $(`#step0${stepCount}`).remove()
+})
+
+
+
+let selectedFile = null
+//上傳圖片
+$(document).on('click', '.img-load', function () {
+    //將點擊的img元素賦值給一個變數
+    const imgElement = $(this);
+    //設定file-load的data將imgElement的元素裝到targerImg這個容器裡
+    $('#file-load').data('targetImg', imgElement)
+    $('#file-load').click();
+});
+$('#file-load').on('change', function (event) {
+    //選取使用者選擇的第一個檔案
+    selectedFile = event.target.files[0]
+    const targetImg = $(this).data('targetImg')
+    //將圖片載入到步驟的圖片
+    if (selectedFile && targetImg) {
+        //讀取檔案
+        const reader = new FileReader();
+        reader.onload = function (e) { targetImg.attr('src', e.target.result) }
+        reader.readAsDataURL(selectedFile);
+    }
+})
