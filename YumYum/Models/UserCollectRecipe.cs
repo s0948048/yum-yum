4b7c35c3f1d2f8b06c99750722b27c7497f1f10e
FangTzu-Chiang <onion110324@gmail.com>
2024-11-20 20:26:00 +0800

    feat:Add table

---
+++ YumYum/Models/UserCollectRecipe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YumYum.temp;
+
+public partial class UserCollectRecipe
+{
+    public int UserId { get; set; }
+
+    public short RecipeId { get; set; }
+}
--- YumYum/Models/YumYumDbContext.cs
+++ YumYum/Models/YumYumDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using YumYum.temp;
 
 namespace YumYum.Models;
 
@@ -65,9 +66,9 @@ public partial class YumYumDbContext : DbContext
 
     public virtual DbSet<UserSecretInfo> UserSecretInfos { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=yumyumdb;Integrated Security=True;Encrypt=False;");
+
+
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -584,6 +585,7 @@ public partial class YumYumDbContext : DbContext
                 .IsUnicode(false);
             entity.Property(e => e.UserNickname).HasMaxLength(20);
 
+            // Many-to-many relationship configuration
             entity.HasMany(d => d.Recipes).WithMany(p => p.Users)
                 .UsingEntity<Dictionary<string, object>>(
                     "UserCollectRecipe",
@@ -604,8 +606,11 @@ public partial class YumYumDbContext : DbContext
                     });
         });
 
+        
+
         OnModelCreatingPartial(modelBuilder);
     }
 
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
 }
