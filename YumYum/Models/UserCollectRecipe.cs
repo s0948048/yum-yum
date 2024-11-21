f6273bebda13f7b1532754e4ce25881ad897bebe
Hubery-1003 <k25298768@gmail.com>
2024-11-21 10:50:31 +0800

    fix:UserCollectRecipe Error

--- YumYum/Models/UserCollectRecipe.cs
+++ YumYum/Models/UserCollectRecipe.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
-namespace YumYum.temp;
-
-public partial class UserCollectRecipe
+namespace YumYum.Models
 {
-    public int UserId { get; set; }
+    public partial class UserCollectRecipe
+    {
+        [Key]
+        [Column(Order = 1)]
+        public int UserID { get; set; }
 
-    public short RecipeId { get; set; }
+        [Key]
+        [Column(Order = 2)]
+        public short RecipeID { get; set; }
+        
+        [ForeignKey(nameof(RecipeID))] // 綁定外鍵
+        public RecipeBrief Recipe { get; set; } = null!;
+        
+        [ForeignKey(nameof(UserID))] // 綁定外鍵
+        public UserSecretInfo User { get; set; } = null!;
+    }
 }
