ae753095fa48f02746ffe2b39e5524b5e54658f9
Mogray65 <141423094+Mogray65@users.noreply.github.com>
2024-11-26 07:02:34 +0800

    feat: add validation, modify CherishContactViewModel

--- YumYum/Models/ViewModels/CherishContactViewModel.cs
+++ YumYum/Models/ViewModels/CherishContactViewModel.cs
@@ -5,26 +5,35 @@ namespace YumYum.Models.ViewModels
 	public class CherishContactViewModel
 	{
 		public int GiverUserId { get; set; }
+
+		[Required(ErrorMessage = "必填")]
+		[StringLength(10, ErrorMessage = "暱稱長度不能超過10個字元")]
 		[Display(Name = "暱稱")]
 		public string? UserNickname { get; set; }
+
+		[Required(ErrorMessage = "必填")]
 		public string? TradeCityKey { get; set; }
+
 		public string? CityName { get; set; }
+
+		[Required(ErrorMessage = "必填")]
 		public short TradeRegionId { get; set; }
+
 		public string? RegionName { get; set; }
+
 		[Display(Name = "LINE")]
+		[StringLength(50, ErrorMessage = "LINE ID長度不能超過50個字元")]
+
 		public string? ContactLine { get; set; }
+
 		[Display(Name = "電話號碼")]
+		[StringLength(15, ErrorMessage = "電話號碼長度不能超過15個字元")]
+
 		public string? ContactPhone { get; set; }
+
 		[Display(Name = "其他聯絡方式")]
+		[StringLength(30, ErrorMessage = "其他聯絡方式長度不能超過30個字元")]
+
 		public string? ContactOther { get; set; }
 	}
-
-	//var info = from o in _context.CherishDefaultInfos
-	//		   where o.GiverUserId == userId
-	//		   select o;
-	//ViewBag.DefaultInfo = info.Single();
-
-	//		var city = from o in _context.Cities
-	//				   select o;
-	//ViewBag.CityList = city.ToList();
 }
